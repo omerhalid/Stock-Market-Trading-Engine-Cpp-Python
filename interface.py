f8ae3891a67c4b08e69fd965a270c1be69b5a98c
omerhalid <omerhalidcinar@gmail.com>
2023-12-22 21:06:52 +0300

    Add .gitignore and FastAPI interface

---
+++ interface.py
@@ -0,0 +1,46 @@
+# this is the FastAPI interface for the model
+
+# import libraries
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get("/")
+async def show_stock_names():
+    stocks = {
+        '1': 'Apple Inc. - AAPL',
+        '2': 'Microsoft Corporation - MSFT',
+        '3': 'Amazon.com, Inc. - AMZN',
+        '4': 'Facebook, Inc. - FB',
+        '5': 'Alphabet Inc. (Google) - GOOGL',
+        '6': 'Tesla, Inc. - TSLA',
+        '7': 'NVIDIA Corporation - NVDA',
+        '8': 'PayPal Holdings, Inc. - PYPL',
+        '9': 'Netflix, Inc. - NFLX',
+        '10': 'Adobe Inc. - ADBE',
+        '11': 'Intel Corporation - INTC',
+        '12': 'Cisco Systems, Inc. - CSCO',
+        '13': 'Comcast Corporation - CMCSA',
+        '14': 'PepsiCo, Inc. - PEP',
+        '15': 'Adobe Inc. - ADBE',
+        '16': 'Broadcom Inc. - AVGO',
+        '17': 'Texas Instruments Incorporated - TXN',
+        '18': 'QUALCOMM Incorporated - QCOM',
+        '19': 'Costco Wholesale Corporation - COST',
+        '20': 'Starbucks Corporation - SBUX',
+        '21': 'Amgen Inc. - AMGN',
+        '22': 'Charter Communications, Inc. - CHTR',
+        '23': 'Gilead Sciences, Inc. - GILD',
+        '24': 'Mondelez International, Inc. - MDLZ',
+        '25': 'Automatic Data Processing, Inc. - ADP'
+    }
+    return stocks
+    
+
+@app.get("/{symbol}")
+async def get_current_price(symbol: str):
+    ts = TimeSeries(key=api_key, output_format='json')
+    data, _ = ts.get_quote_endpoint(symbol=symbol)
+    return {"symbol": symbol, "price": data['05. price']}
